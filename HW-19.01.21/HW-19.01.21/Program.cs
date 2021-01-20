712f1d9f29dc52bb36bab087d536457fe172ac1c
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-01-20 19:50:45 +0500

    All Hw for Alif

---
+++ HW-19.01.21/HW-19.01.21/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_19._01._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
