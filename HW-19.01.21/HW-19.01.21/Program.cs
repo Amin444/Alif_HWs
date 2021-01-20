3f36b86371d84c4c25a6a25325d8b312e625f174
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-01-20 20:16:33 +0500

    Solved 2 Tasks

--- HW-19.01.21/HW-19.01.21/Program.cs
+++ HW-19.01.21/HW-19.01.21/Program.cs
@@ -6,7 +6,29 @@ namespace HW_19._01._21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           /*1. Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то
+           есть квадратный корень из их произведения: √a·b.*/
+
+           // double a= 16.8, b = 12.4;
+
+           // double res=Math.Sqrt(a*b);
+           // System.Console.WriteLine($"Result: {res}");
+
+           //////////////////////////////////////////////////////////////////
+
+           /*2. Даны три точки A, B, C на числовой оси. Найти длины отрезков AC и BC и их сумму*/
+
+           // double a =Math.Abs(1.4),b= Math.Abs(-5.5),c=Math.Abs(0.6);
+
+           // double AC=a-c;
+           // double BC=b+c;
+           // double Result=AC+BC;
+           // System.Console.WriteLine($" AC: {AC} \n BC: {BC} \n Result: {Result}");
+
+           //////////////////////////////////////////////////////////////////////////////
+
+           
+
         }
     }
 }
