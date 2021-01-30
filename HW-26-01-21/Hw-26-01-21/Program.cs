3449f22e12bbb8d750084f00ea0598d6e38119d0
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-01-30 22:31:02 +0500

    solved all task

--- HW-26-01-21/Hw-26-01-21/Program.cs
+++ HW-26-01-21/Hw-26-01-21/Program.cs
@@ -7,7 +7,7 @@ namespace HW_26_01021
         static void Main(string[] args)
         {
             // HW 1  //task1 
-            System.Console.Write("Enter the side1 and side2 : ");
+         /*   System.Console.Write("Enter the side1 and side2 : ");
             var rec =new Rectangle(double.Parse(Console.ReadLine()),double.Parse(Console.ReadLine()));
            System.Console.WriteLine($"Area : {rec.AreaCalculator()}");
            System.Console.WriteLine($"Perimeter {rec.PerimeterCalculator()}");
@@ -45,7 +45,16 @@ namespace HW_26_01021
 
                    //task 1
                    Converter converter=new Converter();
-                   converter.Convertations();
+                   converter.Convertations(); */
+
+                    /////////////////////////////////////////////////////////////////////////////////////////////
+
+                    // task 2
+
+                     var em =new Employee();
+                     em.Allsalery();
+                       
+                   
         }
 
                     
@@ -80,11 +89,7 @@ namespace HW_26_01021
 
        class BooK
        {
-            public static void AllShow()
-            {
           
-           
-            }
        }
 
        
@@ -148,31 +153,20 @@ namespace HW_26_01021
                 
             }
 
-
-
-
-       class Employee
+      public class Converter
        {
-           string Name , FirstName;
-          
-          
-
-            public Employee(string Name , string FirstName)
-            {
-                this.Name =Name;
-                this.FirstName =FirstName;
                
-              
-            }
-            public void salary(string Name , string FirstName){
-                        
-            }
-       }
+                double usd,eur,rub,som;
 
-       class Converter
-       {
-           
-                           public void Convertations()
+                public Converter(double eur, double rub, double usd , double som){
+                    this.eur=eur;
+                    this.rub=rub;
+                    this.som=som;
+                    this.usd=usd;
+                }
+                public Converter(){}
+                public double result { get; init; }
+             public void Convertations()
                 {
                     Console.ForegroundColor =ConsoleColor.Red;
                         System.Console.WriteLine("Wellcome to currency conveter! if you want to exit from this programm write 'exit' ");
@@ -187,59 +181,62 @@ namespace HW_26_01021
                    {
                        case "usd":
                        {
-                           double res =0;
+                           
                            System.Console.Write("Enter the amount usd :");
                            double sum =double.Parse(Console.ReadLine());
                             Console.ForegroundColor =ConsoleColor.Yellow;
                            System.Console.Write("what currency do you want to convert : if eur write => e or rub => r or som => s :");
                            Console.ForegroundColor =ConsoleColor.White;
                            string q =Console.ReadLine();
-                           if(q.ToLower().Equals("e")){res =0.8238*sum; System.Console.WriteLine($"Your amount in eur : {res}");}
-                           if(q.ToLower().Equals("r")){res =75.81*sum; System.Console.WriteLine($"Your amount in rub : {res}");}
-                           if(q.ToLower().Equals("s")){res =11.38*sum; System.Console.WriteLine($"Your amount in som : {res}");}
+                           if(q.ToLower().Equals("e")){eur =0.8238*sum; System.Console.WriteLine($"Your amount in eur : {eur}");}
+                           if(q.ToLower().Equals("r")){rub =75.81*sum; System.Console.WriteLine($"Your amount in rub : {rub}");}
+                           if(q.ToLower().Equals("s")){som =11.38*sum; System.Console.WriteLine($"Your amount in som : {som}");}
+                        
                        }break;
 
                        case "eur":
                        {
-                           double res =0;
+                           
                            System.Console.Write("Enter the amount eur :");
                            double sum =double.Parse(Console.ReadLine());
                             Console.ForegroundColor =ConsoleColor.Yellow;
                            System.Console.Write("what currency do you want to convert : if usd write => u or rub => r or som => s :");
                            Console.ForegroundColor =ConsoleColor.White;
                            string q =Console.ReadLine();
-                           if(q.ToLower().Equals("u")){res =1.21*sum; System.Console.WriteLine($"Your amount in usd : {res}");}
-                           if(q.ToLower().Equals("r")){res =92.03*sum; System.Console.WriteLine($"Your amount in rub : {res}");}
-                           if(q.ToLower().Equals("s")){res =13.82*sum; System.Console.WriteLine($"Your amount in som : {res}");}
+                           if(q.ToLower().Equals("u")){usd =1.21*sum; System.Console.WriteLine($"Your amount in usd : {usd}");}
+                           if(q.ToLower().Equals("r")){rub =92.03*sum; System.Console.WriteLine($"Your amount in rub : {rub}");}
+                           if(q.ToLower().Equals("s")){som =13.82*sum; System.Console.WriteLine($"Your amount in som : {som}");}
+                       
                        }break;
 
                        case "rub":
                        {
-                            double res =0;
-                           System.Console.Write("Enter the amount eur :");
+                            
+                           System.Console.Write("Enter the amount rub :");
                            double sum =double.Parse(Console.ReadLine());
                              Console.ForegroundColor =ConsoleColor.Yellow;
                            System.Console.Write("what currency do you want to convert : if usd write => u or eur => e or som => s :");
                             Console.ForegroundColor =ConsoleColor.White;
                            string q =Console.ReadLine();
-                           if(q.ToLower().Equals("u")){res =0.013*sum; System.Console.WriteLine($"Your amount in usd : {res}");}
-                           if(q.ToLower().Equals("e")){res =0.011*sum; System.Console.WriteLine($"Your amount in rub : {res}");}
-                           if(q.ToLower().Equals("s")){res =0.15*sum; System.Console.WriteLine($"Your amount in som : {res}");}
-
+                           if(q.ToLower().Equals("u")){usd =0.013*sum; System.Console.WriteLine($"Your amount in usd : {usd}");}
+                           if(q.ToLower().Equals("e")){eur =0.011*sum; System.Console.WriteLine($"Your amount in rub : {eur}");}
+                           if(q.ToLower().Equals("s")){som =0.15*sum; System.Console.WriteLine($"Your amount in som : {som}");}
+                           
                        }break;
 
                        case "som":
                        {
-                             double res =0;
+                             
                            System.Console.Write("Enter the amount som :");
                            double sum =double.Parse(Console.ReadLine());
                             Console.ForegroundColor =ConsoleColor.Yellow;
                            System.Console.Write("what currency do you want to convert : if usd write => u or rub => r or eur => e :");
                             Console.ForegroundColor =ConsoleColor.White;
                            string q =Console.ReadLine();
-                           if(q.ToLower().Equals("u")){res =0.088*sum; System.Console.WriteLine($"Your amount in usd : {res}");}
-                           if(q.ToLower().Equals("r")){res =6.66*sum; System.Console.WriteLine($"Your amount in rub : {res}");}
-                           if(q.ToLower().Equals("e")){res =0.072*sum; System.Console.WriteLine($"Your amount in som : {res}");}
+                           if(q.ToLower().Equals("u")){usd =0.088*sum; System.Console.WriteLine($"Your amount in usd : {usd}");}
+                           if(q.ToLower().Equals("r")){rub =6.66*sum; System.Console.WriteLine($"Your amount in rub : {rub}");}
+                           if(q.ToLower().Equals("e")){eur =0.072*sum; System.Console.WriteLine($"Your amount in som : {eur}");}
+                           
                        }break;
 
                        case "exit":{dsd =false;}break;
@@ -247,7 +244,38 @@ namespace HW_26_01021
                        {System.Console.WriteLine("Error! try again");}break;
                    }
                    }
+                 
                 }
-       }
-    
-}
+
+        }
+                class Employee
+                {
+                    public string Name { get; set; }
+                    public string LastName { get; set; }
+                    public string position { get; set; }
+                    public int Salary { get; set; }
+
+                    public Employee(string Name, string LastName, string position,int Salary)
+                    {
+                            this.Name =Name;
+                            this.LastName=LastName;
+                            this.position =position;
+                            this.Salary=Salary;
+                    }
+                    public Employee(){}
+
+                    public void Allsalery()
+                    {
+                       Name = "Amin"; LastName ="Mirzoev";position ="programmer";Salary =20000;
+                        System.Console.WriteLine($" Name of Employee : { Name} ");
+                        System.Console.WriteLine($" LastName of Employee : { LastName}");
+                        System.Console.WriteLine($" position of Employee : { position }");
+                        System.Console.WriteLine($" Salery of Employee : { Salary }");
+                         double res =Salary * 0.14;
+                         System.Console.WriteLine($"Your taxes : {res}");
+                    }
+                }
+
+       
+             
+
