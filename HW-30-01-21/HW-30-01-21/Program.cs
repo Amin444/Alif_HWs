cd4f705260b4996153970532ec4d80f944b5700e
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-02-03 20:58:36 +0500

    solved all tasks

--- HW-30-01-21/HW-30-01-21/Program.cs
+++ HW-30-01-21/HW-30-01-21/Program.cs
@@ -6,26 +6,42 @@ namespace HW_30_01_21
     {
         static void Main(string[] args)
         {
-            string sign =Console.ReadLine();
-           string pro ="ProAdmin";
-           string exp ="ExpAdmin";
-
            var DW =new DocumentWorker();
-           DW.OpenDocument();
-           DW.EditDocument();
-           DW.SaveDocument();
+           var PDW =new ProDocumentWorker();
+           var EDW =new ExpertDocumentWorker();
+
+            System.Console.WriteLine("Enter the key : ");
+             string sign =Console.ReadLine();
+             string pro ="ProAdmin";
+             string exp ="ExpAdmin";
+
+          
            if(sign.Equals(pro))
            {
-               var PDW =new ProDocumentWorker();
+               PDW.OpenDocument();
                PDW.EditDocument();
                PDW.SaveDocument();
            }
            else if (sign.Equals(exp))
            {
-               var EDW =new ExpertDocumentWorker();
+               EDW.OpenDocument();
+               EDW.EditDocument();
                EDW.SaveDocument();
            }
+           else
+           {
+                DW.OpenDocument();
+                DW.EditDocument();
+                DW.SaveDocument();
+           }
+           var p =new Player();
+           p.Play();
+           p.Pause();
+           p.Stop();
+           p.Record();
+           
         }
+        
     }
     class DocumentWorker
     {
@@ -43,22 +59,65 @@ namespace HW_30_01_21
             System.Console.WriteLine("Сохранение документа доступно в версии Про");
         }
     }
-    class ProDocumentWorker
+    class ProDocumentWorker : DocumentWorker 
     {
+        new
          public void EditDocument()
         {
             System.Console.WriteLine("Документ отредактирован");
         }
-         public void SaveDocument()
+        new
+         public void  SaveDocument()
         {
             System.Console.WriteLine("Документ сохранен в старом формате, сохранение в остальныхформатах доступно в версии Эксперт");
         }
     }
-    class ExpertDocumentWorker
+    class ExpertDocumentWorker : ProDocumentWorker
     {
+        new
          public void SaveDocument()
         {
             System.Console.WriteLine("Документ сохранен в новомформате");
         }
+    }
+
+        class Player : IPlayable , IRecodable
+        {
+             public void Play()
+        {
+            Console.WriteLine("Play");
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine("Pause");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+         public void Record()
+        {
+            Console.WriteLine("Record");
+        }
+        }
+    interface IPlayable
+    {
+             void Play();
+         
+             void Pause();
+           
+             void Stop();
+           
+    }
+    interface IRecodable
+    {
+             void Record();
+           
+             void Pause();
+           
+             void Stop();
+           
     }
 }
