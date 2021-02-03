a8e673965526a3fb4e2b9be1b515dbc0f8557952
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-02-03 19:30:58 +0500

    make Hw

---
+++ HW-02-02-21/ClassDB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    class Tech
+    {
+        public string model { get; set; }
+        public double price { get; set; }
+        public string Company { get; set; }
+        public int ReleaseDate { get; set; }
+    }
+}
