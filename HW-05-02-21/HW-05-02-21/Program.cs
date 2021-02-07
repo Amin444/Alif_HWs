0adcc140577d608b8175b11c3608ad90b073b5f9
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-02-07 21:44:02 +0500

    Solved HW

---
+++ HW-05-02-21/HW-05-02-21/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace HW_05_02_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+                 
+                    var choose =Console.ReadLine().ToUpper();
+                    switch (choose)
+                    {
+                        case "R":{SelectAll();}break;
+                        case "С":{Insert();}break;
+                        case "U":{Update();}break;
+                        
+                    }
+        }
+        public static void SelectAll()
+        {
+                    string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = $" SELECT * FROM Persons";
+                     var reader = command.ExecuteReader();
+                        while (reader.Read())
+                    {
+                         Console.WriteLine($"" +
+                        $"Id:{reader["Id"]}, \t" +
+                        $"FirstName:{reader["FirstName"]}, \t" +
+                        $"LastName:{reader["LastName"]}, \t" +
+                        $"MiddleName:{reader["MiddleName"]}, \t" +
+                        $"BithDate:{reader["BithDate"]}, \t");
+                    }
+         }
+
+         public static void Insert()
+         {
+                    string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+
+                    var FirstName = Console.ReadLine();
+                    var LastName = Console.ReadLine();
+                    var MiddleName = Console.ReadLine();
+                    var BithDate = Console.ReadLine();
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "Insert into Persons(" +
+                    "FirstName," +
+                    "LastName," +
+                    "MiddleName," +
+                    "BithDate) Values(" +
+                    $"'{FirstName}'," +
+                    $"'{LastName}'," +
+                    $"'{MiddleName}'," +
+                    $"'{BithDate}')"
+                    ;
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Editing was coreccly!");
+                }
+            }
+
+            public static void Update()
+            {
+                  string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+                         var LastName = Console.ReadLine();
+                         var FirstName = Console.ReadLine();
+                         var MiddleName = Console.ReadLine();
+                         var BithDate = Console.ReadLine();
+                        var id =int.Parse(Console.ReadLine());
+
+                     SqlCommand command = connection.CreateCommand();
+                     command.CommandText = $"Update Persons SET LastName='{LastName}',"+
+                       $"FisrtName ='{FirstName}',"+
+                       $"MiddleName ='{MiddleName}',"+
+                       $"BithDate ='{BithDate}' "+
+                       $"where id = {id};";
+
+                         var reader = command.ExecuteReader();
+                        while (reader.Read())
+                    {
+                         Console.WriteLine($"" +
+                        $"Id:{reader["Id"]}, \t" +
+                        $"FirstName:{reader["FirstName"]}, \t" +
+                        $"LastName:{reader["LastName"]}, \t" +
+                        $"MiddleName:{reader["MiddleName"]}, \t" +
+                        $"BithDate:{reader["BithDate"]}, \t");
+                    }
+
+            }
+    
+         
+    
+    }
+}        
+
+
--- HW-30-01-21/HW-30-01-21/Program.cs
+++ HW-30-01-21/HW-30-01-21/Program.cs
@@ -6,118 +6,21 @@ namespace HW_30_01_21
     {
         static void Main(string[] args)
         {
-           var DW =new DocumentWorker();
-           var PDW =new ProDocumentWorker();
-           var EDW =new ExpertDocumentWorker();
-
-            System.Console.WriteLine("Enter the key : ");
-             string sign =Console.ReadLine();
-             string pro ="ProAdmin";
-             string exp ="ExpAdmin";
-
-          
-           if(sign.Equals(pro))
-           {
-               PDW.OpenDocument();
-               PDW.EditDocument();
-               PDW.SaveDocument();
-           }
-           else if (sign.Equals(exp))
-           {
-               EDW.OpenDocument();
-               EDW.EditDocument();
-               EDW.SaveDocument();
-           }
-           else
-           {
-                DW.OpenDocument();
-                DW.EditDocument();
-                DW.SaveDocument();
-           }
-           var p =new Player();
-           p.Play();
-           p.Pause();
-           p.Stop();
-           p.Record();
+        
+          Person<string,int> p =new Person<string,int>
+          {
+                FisrtName ="jhgfd",
+                LastName ="jhgfd",
+                Age =43
+          };
            
+        }
+        class Person<T, TA>
+        {
+            public T FisrtName { get; set; }
+            public T LastName { get; set; }
+            public TA Age { get; set; }
         }
         
-    }
-    class DocumentWorker
-    {
-        public void OpenDocument()
-        {
-            System.Console.WriteLine("Документ открыт");
-        }
-        public void EditDocument()
-        {
-            System.Console.WriteLine("Редактирование документа доступно вверсии Про");
-        }
-        
-         public void SaveDocument()
-        {
-            System.Console.WriteLine("Сохранение документа доступно в версии Про");
-        }
-    }
-    class ProDocumentWorker : DocumentWorker 
-    {
-        new
-         public void EditDocument()
-        {
-            System.Console.WriteLine("Документ отредактирован");
-        }
-        new
-         public void  SaveDocument()
-        {
-            System.Console.WriteLine("Документ сохранен в старом формате, сохранение в остальныхформатах доступно в версии Эксперт");
-        }
-    }
-    class ExpertDocumentWorker : ProDocumentWorker
-    {
-        new
-         public void SaveDocument()
-        {
-            System.Console.WriteLine("Документ сохранен в новомформате");
-        }
-    }
-
-        class Player : IPlayable , IRecodable
-        {
-             public void Play()
-        {
-            Console.WriteLine("Play");
-        }
-
-        public void Pause()
-        {
-            Console.WriteLine("Pause");
-        }
-
-        public void Stop()
-        {
-            Console.WriteLine("Stop");
-        }
-         public void Record()
-        {
-            Console.WriteLine("Record");
-        }
-        }
-    interface IPlayable
-    {
-             void Play();
-         
-             void Pause();
-           
-             void Stop();
-           
-    }
-    interface IRecodable
-    {
-             void Record();
-           
-             void Pause();
-           
-             void Stop();
-           
     }
 }
