13d786355ca35070033202baf70fb554300a99fd
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-02-09 16:01:50 +0500

    Solved Crud

--- HW-05-02-21/HW-05-02-21/Program.cs
+++ HW-05-02-21/HW-05-02-21/Program.cs
@@ -15,9 +15,11 @@ namespace HW_05_02_21
                     var choose =Console.ReadLine().ToUpper();
                     switch (choose)
                     {
+                        case "C":{Insert();}break;
                         case "R":{SelectAll();}break;
-                        case "С":{Insert();}break;
                         case "U":{Update();}break;
+                        case "D":{Delete();}break;
+                        case "S":{SelectById();}break;
                         
                     }
         }
@@ -67,7 +69,7 @@ namespace HW_05_02_21
                 {
                     System.Console.WriteLine("Editing was coreccly!");
                 }
-            }
+        }
 
             public static void Update()
             {
@@ -81,8 +83,8 @@ namespace HW_05_02_21
                         var id =int.Parse(Console.ReadLine());
 
                      SqlCommand command = connection.CreateCommand();
-                     command.CommandText = $"Update Persons SET LastName='{LastName}',"+
-                       $"FisrtName ='{FirstName}',"+
+                     command.CommandText = $"UPDATE Persons SET LastName='{LastName}',"+
+                       $"FirstName ='{FirstName}',"+
                        $"MiddleName ='{MiddleName}',"+
                        $"BithDate ='{BithDate}' "+
                        $"where id = {id};";
@@ -99,8 +101,44 @@ namespace HW_05_02_21
                     }
 
             }
+
+            public static void Delete()
+            {
+                   string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+                    var id =int.Parse(Console.ReadLine());
+                    SqlCommand command = connection.CreateCommand();
+                     command.CommandText = $"DELETE from Persons where id ={id};";
+
+                    var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("deleteing was successfuly!");
+                }
+
+            }
     
-         
+         public static void SelectById()
+         {
+                    string ConnString = @"Data source=MACHINE-PGO7H84\SQL2017; initial catalog=People; integrated security=true;";
+                    SqlConnection connection = new SqlConnection(ConnString);
+                    connection.Open();
+                      var id =int.Parse(Console.ReadLine());
+                     SqlCommand command = connection.CreateCommand();
+                     command.CommandText = $"SELECT * from Persons where id ={id};";
+                
+                      var reader = command.ExecuteReader();
+                        while (reader.Read())
+                    {
+                         Console.WriteLine($"" +
+                        $"Id:{reader["Id"]}, \t" +
+                        $"FirstName:{reader["FirstName"]}, \t" +
+                        $"LastName:{reader["LastName"]}, \t" +
+                        $"MiddleName:{reader["MiddleName"]}, \t" +
+                        $"BithDate:{reader["BithDate"]}, \t");
+                    }
+         }
     
     }
 }        
