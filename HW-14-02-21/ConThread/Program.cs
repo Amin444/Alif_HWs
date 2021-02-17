d949dd64098222891d59b863924146e7f3ec0c36
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-02-17 20:20:39 +0500

    Matrix

---
+++ HW-14-02-21/ConThread/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+namespace Threads
+{
+    class Program
+    {
+         public static List<Client> lastclient = new List<Client>();
+        public static List<Client> listclient = new List<Client>()
+        {
+            new Client(){
+                id=1,
+                Balance=124
+            },
+            new Client(){
+                id=2,
+                Balance=12.44m
+            },
+            new Client(){
+                id=3,
+                Balance=124.423m
+            },
+            new Client(){
+                id=4,
+                Balance=194.4m
+            },
+            new Client(){
+                id=5,
+                Balance=1.24m
+            },
+            new Client(){
+                id=6,
+                Balance=53.534m
+            }
+        };
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите число: ");
+            string choose =Console.ReadLine();
+            switch(choose)
+            {
+                case "1":{Thread threadINsert = new Thread(new ThreadStart(Insert( id:n,  Balance: 533m)));  threadINsert.Start();}  break;
+                case "2":{Thread threadSelect = new Thread(new ThreadStart(Select));  threadSelect.Start();} break;
+            }
+            
+        }
+
+        public static void Insert(int id , decimal Balance)
+        
+        {
+           System.Console.Write("Enter the id : ");
+           int nId =int.Parse(Console.ReadLine());
+           Console.Write("Enter the Balance :");
+           decimal nBalance =decimal.Parse(Console.ReadLine());
+           lastclient.Add(new Client(){id=nId, Balance=nBalance});
+
+                if(nId>0 && nBalance>0)
+                {
+                    System.Console.WriteLine("Add Succesfuly");
+                }
+            
+        }
+        public static void Update()
+        {
+            var cId =int.Parse(Console.ReadLine());
+            listclient.FindAll(s=>s.id==cId);
+            var nBalance=decimal.Parse(Console.ReadLine());
+            listclient.ForEach(x=>x.id=cId);
+            lastclient.ForEach(x=>x.Balance=nBalance);
+        }
+        public static void Delete()
+        {
+
+        }
+        public static void Select()
+        {
+                      foreach (var item in lastclient)
+            {
+                System.Console.WriteLine($"id : {item.id}, Balance : {item.Balance}");
+            }
+        }
+
+    }
+    class Client
+    {
+        public int id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ HW_16_02_21/Matrix/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          List<shifts> mylist=new List<shifts>();
+            while(true)
+            {
+                
+                Task.Run(
+                ()=>
+                {
+                    for (int i = 0; i < new Random().Next(100, 1000); i++)
+                    {
+                        mylist.Add(new shifts()
+                        {
+                            left=new Random().Next(100),
+                            Top = new Random().Next(3,8),
+                            simvol=letters(),
+                        }
+                        
+                        );
+                    }
+                }
+                ).Wait();
+
+                     Task.Run(() =>
+                {
+                    lettersRain(mylist);
+                    mylist.Clear();
+
+                }).Wait();
+
+            }
+        }
+         
+        public static char[] letters()
+        {
+            string letters="";
+            for (char i = 'A'; i <= 'z'; i++)
+            {
+             if ((char)new Random().Next(0,4)==1)
+             {
+                 letters+=i;
+             }   
+            }
+            return letters.ToCharArray();
+        }
+
+       public static void lettersRain(List<shifts> Rain)
+       {
+            int count=0;
+            for (int i = 0; i < 10; i++)
+            {
+                foreach(var A in Rain )
+              {
+                    Console.CursorTop = i + A.Top;
+                foreach (var item in A.simvol)
+                {
+                    Console.CursorLeft=A.left;
+                    if (count==A.simvol.Length-1)
+                    {
+                        Console.ForegroundColor=ConsoleColor.White;
+                    }
+                    else if (count==A.simvol.Length-2)
+                    {
+                     Console.BackgroundColor=ConsoleColor.Green;   
+                    }
+                    else
+                    {
+                    Console.BackgroundColor=ConsoleColor.DarkGreen;
+                    }
+                    System.Console.WriteLine(item);
+                    count++;
+                }
+                count =0;
+             }
+            }
+            Thread.Sleep(1000);
+            Console.Clear();
+       }
+        
+    }
+
+    class shifts
+     {
+         public int left{get;set;}
+         public int Top{get;set;} 
+         public char[] simvol { get; set; }
+     }
+}
